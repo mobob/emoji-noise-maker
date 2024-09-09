60189c66ccff3a8cbfb39349c11c1927dd56243c
Bob <bobdurie@gmail.com>
2024-09-08 22:22:37 -0400

    fix: extra import

--- src/components/EmojiNoiseTextureGrid.tsx
+++ src/components/EmojiNoiseTextureGrid.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { EmojiNoiseTextureGridGenerator } from "./EmojiGridGenerator";
 
 // Include the EmojiNoiseTextureGridGenerator class here
